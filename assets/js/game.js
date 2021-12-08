5ffe27128bfab8fa0bd0beaa2b1ef9e480d6de1d
Thoma77s <thomassiffc3@gmail.com>
2021-12-08 10:41:22 -0500

    GAME OVER

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,8 +67,12 @@ var fight = function(enemyName) {
 ;
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50
+        debugger;
+        fight(pickedEnemyName);
+    }
 }
 
